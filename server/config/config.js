7c3ba2d8c16e8c21b4835d05239847e0659e06f6
ronalucard <rona-lucard@outlook.com>
2020-11-17 02:04:07 -0600

    variable entorno bd

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb://localhost:27017/cafe'
+    urlDB = 'mongodb://localhost:27017/cfe'
 } else {
     urlDB = process.env.MONGO_URI;
 }
